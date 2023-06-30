b3463850504764fc7a728f769e2b8e3b7ee4e30d
lxy <lxy3417@cug.edu.cn>
2023-06-30 17:03:35 +0800

    lxy

---
+++ src/test/java/org/example/BrandServiceTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import com.spring.study.config.SpringConfig;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
+public class BrandServiceTest {
+
+}
